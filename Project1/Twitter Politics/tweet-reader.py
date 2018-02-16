d84de25f56fc2dd4f4133a26f425d2920f55a6a8
Victor <ramir193@msu.edu>
2018-02-16 00:22:50 -0500

    Adding some project files

---
+++ Project1/Twitter Politics/tweet-reader.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 15:31:17 2018
+
+@author: ramir
+"""
+import json
+
+with open('btc.json', 'r') as f:
+    d = f.read()
+    print(type(d))
+    
